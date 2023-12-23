eb349ce7bbf529d79fea1f209b6e5674c4a25cb4
AnnaKhiz <kukurika.anna@gmail.com>
2023-12-23 14:05:01 +0200

    done

--- src/index.ts
+++ src/index.ts
@@ -1,178 +1,126 @@
-// Напишіть функцію isString, яка перевірятиме, чи є передане значення рядком.
-// Потім використовуйте її для звуження типу змінної.
+// Фільтрація масиву
+// Напишіть узагальнену функцію filterArray(array, condition), яка фільтрує масив елементів на основі наданої умови.
 
-function changeString(value: unknown): void {
-	if (isString(value)) {
-		value.toLowerCase();
-	} else {
-		throw new Error('Incorrect value type');
+enum ConditionNumberTypesEnum {
+	EVEN = 'even',
+	ODD = 'odd',
+}
+
+enum ConditionStringTypesEnum {
+	UPPER = 'upper',
+	LOWER = 'lower'
+}
+
+function filterArray<TType, TCondition>(array: Array<TType>, condition: TCondition): Array<TType> {
+	if (array.every(isNumberValue)) {
+		return filterNumbers(array, condition);
+	} else if (isStringValue) {
+		return filterStrings(array, condition);
 	}
 }
 
-function isString(value: unknown): value is string {
-	return typeof value === 'string';
-}
-
-// У вас є масив з елементами різних типів. Напишіть функцію, яка приймає цей масив
-// і фільтрує його так, щоб у підсумку в ньому залишилися тільки рядки.
-// Використовуйте захисника типу для цього завдання.
-
-function findString(value: unknown): void {
-	if (isStringInArray(value)) {
-		value.filter(elem => typeof elem === 'string');
-	} else {
-		throw new Error('Incorrect value type');
-	}
-}
-function isStringInArray(value: unknown): value is any[] {
-	return value instanceof Array;
-}
-
-// У вас є об'єкт, який може містити довільні властивості.
-// Напишіть функцію, яка приймає цей об'єкт і повертає значення однієї з
-// властивостей, якщо воно існує і має певний тип.
-
-interface IUser {
-	name: string;
-	age: number;
-	sex: string;
-}
-
-const user: IUser = {
-	name: 'John',
-	age: 33,
-	sex: 'male',
-}
-
-function getUserInfo(user: unknown): string {
-	if (isUserObject(user)) {
-		return `User is ${user.sex}, his name is ${user.name}, ${user.age} years old.`
-	} else {
-		throw new Error('Incorrect value type');
+function filterNumbers<TType, TCondition>(array: Array<TType>, condition: TCondition): Array<TType> {
+	switch (condition) {
+		case 'even':
+			return array.filter((elem: TType) => elem as number % 2 === 0)
+		case 'odd':
+			return array.filter((elem: TType) => elem as number % 2 !== 0)
+		default:
+			throw new Error('no such condition')
 	}
 }
 
-function isUserObject(value: unknown): value is IUser {
-	return (typeof value === 'object'
-		&& 'age' in value
-		&& 'name' in value
-		&& 'sex' in value
-		&& typeof 'sex' === 'string'
-	);
+function filterStrings<TType, TCondition>(array: Array<TType>, condition: TCondition): Array<TType> {
+	switch (condition) {
+		case 'upper':
+			return array.filter((elem: TType) => !(elem === (elem as string).toLowerCase()))
+		case 'lower':
+			return array.filter((elem: TType) => elem === (elem as string).toLowerCase())
+		default:
+			throw new Error('no such condition')
+	}
 }
 
-// Створіть кілька захисників типу, кожен з яких перевіряє певний аспект об'єкта
-// (наприклад, наявність певної властивості або її тип). Потім напишіть функцію, яка
-// використовує цих захисників у комбінації для звуження типу об'єкта до більш конкретного типу.
-
-interface IPerson {
-	name: string;
-	age: number;
+function isNumberValue(value: unknown): value is 'number' {
+	return typeof value === 'number'
 }
 
-interface IStudent extends IPerson {
-	marks: {
-		math: string;
-		music: string;
+function isStringValue(value: unknown): value is 'string' {
+	return typeof value === 'string'
+}
+
+filterArray([1, 5, 2, 8, 9, 15, 48], ConditionNumberTypesEnum.EVEN)
+filterArray([1, 5, 2, 8, 9, 15, 48], ConditionNumberTypesEnum.ODD)
+filterArray(['hello', 'true', 'sun', 'SKY'], ConditionStringTypesEnum.UPPER)
+filterArray(['hello', 'WORLD', 'sun', 'SKY'], ConditionStringTypesEnum.LOWER)
+
+
+// 	Узагальнений стек
+// Створіть узагальнений клас Stack, який являє собою стек елементів з методами push, pop і peek.
+
+interface IMethods<TItem> {
+	arr: Array<TItem>;
+	push(value: TItem): number;
+	pop(): TItem;
+	peek(): TItem;
+}
+
+class Stack<TItem> implements IMethods<TItem>{
+	constructor(public arr: Array<TItem>) { }
+
+	public push(value: TItem): number {
+		return this.arr.push(value)
 	};
-	study(): void;
-}
 
-interface ITeacher extends IPerson {
-	work(): void;
-}
-
-class Student implements IStudent {
-	name: string = 'Bob';
-	age: number = 32;
-	marks: {
-		math: 'good',
-		music: 'excellent'
+	public pop(): TItem {
+		return this.arr.pop()
 	};
-	study(): void { };
+
+	public peek(): TItem {
+		return this.arr.at(-1)
+	};
 }
 
-class Teacher implements ITeacher {
-	name: string = 'Bob';
-	age: number = 32;
-	work(): void { };
+const stack = new Stack([1, 2, 5, 'elem']);
+
+
+// 	Узагальнений словник
+// Створіть узагальнений клас Dictionary, який являє собою словник (асоціативний масив) з методами set, get і has. Обмежте ключі тільки валідними типами для об'єкта
+
+type Type<TValue> = {
+	a: string,
+	b: TValue
 }
 
-function getName(value: any): value is Student {
-	return (
-		'name' in value && typeof 'name' === 'string'
-	);
+interface IDictionaryIndex<TValue> {
+	[key: string]: TValue
+	| ((x?: string, y?: TValue) => any)
+	| Type<TValue>;
 }
 
-function getMarks(value: any): value is Student {
-	return (
-		'marks' in value && typeof 'marks' === 'object'
-	);
-}
+class Dictionary<TValue> implements IDictionaryIndex<TValue>{
+	[key: string]: TValue
+	| ((x?: string, y?: TValue) => any)
+	| Type<TValue>;
 
-function getAction(object: Student | Teacher): void {
-	if (getName(object) && getMarks(object)) {
-		object.study();
-	} else {
-		object.work();
+	private dataArray: Type<TValue>;
+
+	constructor() {
+		this.dataArray = {} as Type<TValue>;
+	}
+
+	public set(key: string, value: TValue): void {
+		this.dataArray[key] = value;
+		console.log(this.dataArray)
+	}
+
+	public get(key: string): TValue | any {
+		return this.dataArray[key]
+	}
+	public has(key: string): boolean {
+		return key in this.dataArray
 	}
 }
 
-// У вас є змінна, яка може бути одного з декількох типів (наприклад, рядок або число).
-// Напишіть функцію, яка приймає цю змінну і виконує довільні операції, специфічні для кожного з типів.
+const dict = new Dictionary<number>()
 
-type Value = string | number;
-
-function makeAction(param: Value): Value {
-	if (typeof param === 'string') {
-		return param.toUpperCase();
-	} else {
-		return ++param;
-	}
-}
-
-
-// Створіть захисник типу, який перевірятиме, чи є передане значення функцією. Потім напишіть функцію,
-// яка використовує цей гард для звуження типу змінної і викликає передану функцію, якщо вона існує.
-
-class SomeClass { };
-const instClass = new SomeClass();
-
-function isFunction(value: unknown): value is Function {
-	return value instanceof Function;
-}
-
-function checkFunction(value: unknown): void {
-	if (isFunction(value)) {
-		value.apply(instClass, [1, 2, 3]);
-	} else {
-		throw new Error('The object is not a function');
-	}
-}
-
-
-// Створіть класи з ієрархією успадкування і потім напишіть функцію, яка використовує захисник
-// типу для звуження типу об'єктів, що базуються на цій ієрархії.
-
-class Cat {
-	eat(): void { };
-	color: string;
-}
-
-class Brit extends Cat {
-	name: string;
-}
-
-function isBrit(value: any): value is Brit {
-	return (
-		'name' in value
-	);
-}
-
-function getCat(cat: Cat | Brit): string {
-	if (isBrit(cat)) {
-		return `Cat ${cat.name}`;
-	} else {
-		return `Cat ${cat.color}`;
-	}
-}
