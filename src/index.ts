670151f6c580c714106cb467b8192071d02a0b17
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-22 11:39:26 +0200

    done

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,151 @@
-// Візьміть декоратор DeprecatedMethod і навчіть його працювати з об'єктом, який вміє приймати причину, 
-// через яку його не варто використовувати, і назву методу, яким його можна замінити, якщо це можливо.
+// Вам необхідно написати додаток Todo list. 
+// У списку нотаток повинні бути методи для додавання нового запису, видалення, 
+// редагування та отримання повної інформації про нотаток за ідентифікатором, а так само отримання списку всіх нотатoк. 
+// Крім цього, у користувача має бути можливість позначити нотаток, як виконаний, і отримання інформації про те, 
+// скільки всього нотаток у списку і скільки залишилося невиконаними. 
+// Нотатки не повинні бути порожніми. 
 
-// Створіть декоратори поля MinLength, MaxLength та Email.
+// Кожний нотаток має назву, зміст, дату створення і редагування та статус. Нотатки бувають двох типів. 
+// Дефолтні та такі, які вимагають підтвердження при ридагуванні (окремі класи). +
 
-// Використайте попередню версію декораторів і зробіть так, щоб їх можно було використовувати разом.
+// Окремо необхідно розширити поведінку списку та додати можливість пошуку нотатка по будь-якому 
+// філду, або у якості опції вказувати по якому саме вести пошук.
 
-class User {
-	public name: string = 'John';
-	public surname: string = 'Doe';
+// Також окремо необхідно розширити список можливістю сортування нотаток за статусом або часом створення.
 
-	@MinLength(10)
-	@MaxLength(20)
-	@SetEmail
-	public email: string = 'someemail@gmail.com';
-
-	@DeprecatedMethod('Old method', 'getFullName')
-	public getName(): void {
-		console.log(`${this.name}`)
-	}
-	public getFullName(): void {
-		console.log(`Full name: ${this.name} ${this.surname}`)
-	}
+enum NoteTypeEnums {
+	DEFAULT = 'default',
+	CONFIRM = 'confirm'
 }
 
-const user = new User();
-user.getName();
-
-function DeprecatedMethod(cause = '', replacement = '') {
-	return function <T, A extends any[], R>(
-		originalMethod: (...args: A) => R,
-		context: ClassMethodDecoratorContext
-	): any {
-		function replacementMethod(this: T, ...args: A): R {
-			console.log(`Method - ${String(context.name)} is deprecated. The cause: ${cause}. Use instead: ${replacement}`)
-			return originalMethod.apply(this, args)
-		}
-		return replacementMethod
-	}
+interface INote {
+	id: number;
+	title: string;
+	body: string;
+	dateCreate: number;
+	dateEdit: number;
+	isFinished: boolean;
 }
 
-function MinLength(min: number) {
-	return function <T, V>(
-		originalProperty: undefined,
-		context: ClassFieldDecoratorContext<T, V>
-	): any {
-		function setProperty(this: T, originalValue: V): V {
-			if (String(originalValue).length >= min) {
-				return originalValue
-			} else {
-				throw new Error(`Min length should be more than - ${min}`)
+interface IEditNote {
+	type: NoteTypeEnums;
+}
+
+abstract class TodoList {
+	public todoList: INote[];
+	public title: string;
+	public body: string;
+
+	protected readonly id: number;
+	protected readonly dateCreate: number;
+	protected dateEdit: number;
+	protected isFinished: boolean;
+
+	constructor(note: INote) {
+		this.todoList = [note];
+		this.id = note.id;
+		this.title = note.title;
+		this.body = note.body;
+		this.dateCreate = +Date.now();
+		this.dateEdit = note.dateEdit;
+		this.isFinished = note.isFinished;
+	}
+
+	public get allTodoListNotes(): INote[] {
+		return this.todoList
+	}
+
+	protected checkAllNotFinishedNotes(): INote[] {
+		return this.todoList.filter(note => note.isFinished === false)
+	}
+
+	protected countAllNotes(): number {
+		return this.todoList.length
+	}
+
+	public finishNote(id: number) {
+		this.todoList.filter(note => note.id === id && note.isFinished === false ? note.isFinished = true : note.isFinished);
+		this.countAllNotes();
+		this.checkAllNotFinishedNotes();
+	}
+
+	public getNoteInfo(id: number): INote[] {
+		return this.todoList.filter(note => note.id === id)
+	}
+
+	public addNote(note: INote): void {
+		this.todoList.push(note)
+	}
+
+	public deleteNote(id: number): INote[] {
+		return this.todoList.filter(element => element.id !== id)
+	}
+
+	public abstract editNote(note: INote): void;
+}
+
+
+class TodoListDefault extends TodoList implements IEditNote {
+	public readonly type: NoteTypeEnums;
+
+	constructor(note: INote, type: NoteTypeEnums.DEFAULT) {
+		super(note)
+		this.type = type;
+	}
+
+	public editNote(note: INote): void {
+		this.todoList.map(element => {
+			if (element.id === note.id) {
+				element.title = note.title;
+				element.body = note.body;
+				element.dateEdit = note.dateEdit;
+				element.isFinished = note.isFinished;
 			}
-		}
-		return setProperty
+		});
 	}
 }
 
-function MaxLength(max: number) {
-	return function <T, V>(
-		originalProperty: undefined,
-		context: ClassFieldDecoratorContext<T, V>
-	): any {
-		function setProperty(this: T, originalValue: V): V {
-			if (String(originalValue).length <= max) {
-				return originalValue
-			} else {
-				throw new Error(`Max length should be less than - ${max}`)
-			}
-		}
-		return setProperty
-	}
-}
+class TodoListConfirm extends TodoList implements IEditNote {
+	public readonly type: NoteTypeEnums.CONFIRM;
 
-function SetEmail<T, V>(
-	originalProperty: undefined,
-	context: ClassFieldDecoratorContext<T, V>
-): any {
-	function setProperty(this: T, originalValue: V): V {
-		if (String(originalValue).includes('@')) {
-			console.log(`Email: ${originalValue}`)
-			return originalValue
+	constructor(note: INote, type: NoteTypeEnums.CONFIRM) {
+		super(note)
+		this.type = type;
+	}
+
+	public editNote(note: INote): void {
+		const result = confirm(`Confirm editing note`)
+		if (result) {
+			this.todoList.map(element => {
+				if (element.id === note.id) {
+					element.title = note.title;
+					element.body = note.body;
+					element.dateEdit = note.dateEdit;
+					element.isFinished = note.isFinished;
+				}
+			})
 		} else {
-			throw new Error(`Email must include "@"`)
+			throw new Error('You didnt confirm editing')
 		}
 	}
-	return setProperty
 }
 
+class FindTodoNote extends TodoList {
+	public editNote(note: INote): void {
+		throw new Error('This class doesnt support editing')
+	}
+
+	public findTodoNote(field: string, value: string | number | boolean): INote[] {
+		return this.todoList.filter(todo => todo[field] === value)
+	}
+}
+
+class SortTodoNote extends TodoList {
+	public editNote(note: INote): void {
+		throw new Error('This class doesnt support editing')
+	}
+
+	public sortTodoNote(status: string): INote[] {
+		return this.todoList.sort((a, b) => String(a[status]).localeCompare(String(b[status])))
+	}
+}
