f53f14154660173d6035612dad33c397ee59a773
AnnaKhiz <kukurika.anna@gmail.com>
2024-01-30 13:47:20 +0200

    done

--- src/index.ts
+++ src/index.ts
@@ -1,133 +1,137 @@
-// Вам необхідно написати додаток Todo list. 
-// У списку нотаток повинні бути методи для додавання нового запису, видалення, 
-// редагування та отримання повної інформації про нотаток за ідентифікатором, а так само отримання списку всіх нотатoк. 
-// Крім цього, у користувача має бути можливість позначити нотаток, як виконаний, і отримання інформації про те, 
-// скільки всього нотаток у списку і скільки залишилося невиконаними. 
-// Нотатки не повинні бути порожніми. 
 
-// Кожний нотаток має назву, зміст, дату створення і редагування та статус. Нотатки бувають двох типів. 
-// Дефолтні та такі, які вимагають підтвердження при ридагуванні (окремі класи). +
+// Вам необхідно розширити поведінку прикладу з банківським рахунком. 
+// Додайте до нашої програми компонент Bank, який вміє створювати ти закривати акаунти для клієнтів. 
+// Кліент може мати декілька аккаунтів з різними типами валют. Bank повинен бути Singleton!
 
-// Окремо необхідно розширити поведінку списку та додати можливість пошуку нотатка по будь-якому 
-// філду, або у якості опції вказувати по якому саме вести пошук.
+//  Для тих, кому цікаво ускладнити - додайте можливість ставити транзацкції у чергу та мати можливість 
+// їх повторювати чи відміняти (Command)
 
-// Також окремо необхідно розширити список можливістю сортування нотаток за статусом або часом створення.
 
-enum NoteTypesEnum {
-	DEFAULT = 'default',
-	CONFIRM = 'confirm'
+interface IBankClient {
+	firstName: string;
+	lastName: string;
 }
 
-interface INote {
-	id: number;
-	title: string;
-	body: string;
-	dateCreate: number;
-	dateEdit: number;
-	isFinished: boolean;
-	finishNote(id: number): void;
-	editNote(note: INote): void;
-}
-
-class TodoList {
-	protected todoList: Array<Note | NoteConfirm> = [];
-
-	public get allTodoListNotes(): (Note | NoteConfirm)[] {
-		return this.todoList
-	}
-
-	protected checkAllNotFinishedNotes(): void {
-		this.todoList.filter(note => note.isFinished === false)
-	}
-
-	protected countAllNotes(): number {
-		return this.todoList.length
-	}
-
-	public finishNote(id: number): void {
-		const note = this.todoList.find(element => element.id === id);
-		if (!note) throw new Error('Incorrect ID');
-
-		note.finishNote()
-	}
-
-	public getNoteInfo(id: number): void {
-		this.todoList.filter(note => note.id === id)
-	}
-
-	public addNote(note: Note | NoteConfirm): void {
-		this.todoList.push(note)
-	}
-
-	public deleteNote(id: number): void {
-		this.todoList.filter(element => element.id !== id)
-	}
-
-	public editNote(id: number, payload: Note): void {
-		const note = this.todoList.find(element => element.id === id);
-
-		if (!note) throw new Error('Incorrect ID');
-
-		note.editNote(payload);
-	};
-
-	public findTodoNote(field: string, value: string | number | boolean): Note | NoteConfirm {
-		const note = this.todoList.find(element => element[field] === value);
-
-		if (!note) throw new Error('Incorrect ID');
-
-		return note
-	}
-
-	public sortTodoList(status: string): void {
-		this.todoList.sort((a, b) => String(a[status]).localeCompare(String(b[status])))
-	}
-
-}
-
-class Note implements INote {
-	public isFinished = false;
+class BankClient implements IBankClient {
+	private _accounts: IAccount[];
 
 	constructor(
-		public readonly id: number,
-		public title: string,
-		public body: string,
-		public readonly dateCreate: number,
-		public dateEdit: number,
-		public type: NoteTypesEnum.DEFAULT
-	) { }
+		private readonly _firstName: string,
+		private readonly _lastName: string,
+		private readonly _bday: number,
+		private _accountNumber: string | null = null,
 
-	public editNote(note: Note): void {
-		Object.assign(this, note);
+	) {
+		this._accounts = [{ iban: '', currency: '' }];
 	}
 
-	public finishNote(): void {
-		this.isFinished = true;
+	public get accountNumber(): string {
+		if (!this._accountNumber) throw new Error('New client');
+		return this._accountNumber;
 	}
+
+	public set accountNumber(value: string) {
+		this._accountNumber = value;
+	}
+
+	public get age(): number {
+		return new Date().getFullYear() - this._bday
+	}
+
+	public get firstName(): string {
+		return this._firstName
+	}
+
+	public get lastName(): string {
+		return this._lastName
+	}
+
 }
 
-class NoteConfirm implements INote {
-	public isFinished = false;
+interface IAccount {
+	iban: string;
+	currency: string;
+}
+
+class BankAccount {
+	private readonly iban: IAccount[];
+	private _isActive: boolean;
 
 	constructor(
-		public readonly id: number,
-		public title: string,
-		public body: string,
-		public readonly dateCreate: number,
-		public dateEdit: number,
-		public type: NoteTypesEnum.CONFIRM
-	) { }
-
-	public editNote(note: INote): void {
-		if (this.isFinished) return;
-		Object.assign(this, note);
+		private readonly holder: IBankClient,
+		private readonly currency: string,
+		private balance = 0
+	) {
+		this.iban = [{ iban: this.createIBAN(this.currency), currency: this.currency }];
+		this._isActive = true;
 	}
 
-	public finishNote(): void {
-		this.isFinished = true;
+	public get holderName(): string {
+		return `${this.holder.lastName} ${this.holder.firstName}`;
 	}
 
-	public confirm(value: boolean): void {
-		this.isFinished = value
+	public get info(): string {
+		return `${this.balance}${this.currency}`;
+	}
+
+	public set close(value: boolean) {
+		this._isActive = value;
+	}
+
+	public createIBAN(currency: string): string {
+		return `${currency}${(Math.random().toFixed(10)).toString().slice(2)}`
+	}
+
+	public deposit(amount: number): void {
+		this.balance += amount;
+	}
+
+	public withdraw(amount: number): void {
+		if (this.balance < amount) throw new Error(`${this.holderName} you dont have enough money for withdraw`)
+
+		this.balance -= amount;
+
+	}
+
+	public getNumber(currency: string): IAccount[] {
+		return this.iban.filter(num => num.currency === 'string' ? num.iban : false)
+	}
+
+	public createAccount(client: IBankClient, account: IAccount) {
+		if (client.firstName !== this.holder.firstName || client.lastName !== this.holder.lastName) {
+			throw new Error('No such client')
+		} else {
+			this.iban.push(account)
+		}
+	}
+
+	
+}
+
+const bankAccount = new BankAccount({ firstName: 'John', lastName: 'Doe' }, 'UAH', 0)
+
+
+bankAccount.createAccount({ firstName: 'John', lastName: 'Doe' }, { iban: bankAccount.createIBAN('US'), currency: 'USD' })
+// bankAccount.createAccount({ firstName: 'John', lastName: 'Doe' }, { iban: bankAccount.createIBAN('EU'), currency: 'EUR' })
+// bankAccount.createAccount({ firstName: 'John', lastName: 'Doe' }, { iban: bankAccount.createIBAN('UA'), currency: 'UAH' })
+bankAccount.info
+bankAccount.close = false
+bankAccount.info
+
+//Singleton
+class Bank {
+	private static _bankInstance: Bank;
+
+	public static getBankInstance() {
+		if (!Bank._bankInstance) {
+			Bank._bankInstance = new Bank(bankAccount)
+		}
+		return Bank._bankInstance
+	}
+
+	private constructor(private account: BankAccount | BankAccount[]) {
 	}
 }
+
+const bank = Bank.getBankInstance();
+
